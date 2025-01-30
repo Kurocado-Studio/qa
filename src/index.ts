36ec1fff263e4316b2ed317fecefdef019ae2882
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-01-30 15:23:14 -0600

    feat(config): a11y test utils for remix

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ export {
   remixVitestConfig,
 } from './remix/remixVitestConfig';
 
+export { renderComponent, auditComponentA11y } from './remix/utils';
+
 export * from '@testing-library/react';
+
 export type * from '@testing-library/react';
 
 export { type VitestConfig, vitestConfig } from './common/vitestConfig';
