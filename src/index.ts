ac864c932994eac4fb0f1417220250102deb8d8e
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-01-30 14:53:11 -0600

    fix(config): renames export paths

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ export {
   remixVitestConfig,
 } from '~/remix/remixVitestConfig';
 
+export * from '@testing-library/react';
+export type * from '@testing-library/react';
+
 export { type VitestConfig, vitestConfig } from '~/common/vitestConfig';
 
 export { defineConfig as defineVitestConfig } from 'vitest/config';
