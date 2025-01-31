62ef11db2e6bf3d559f9aa22f3e0de65ba306ec1
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-01-31 14:29:11 -0600

    fix(deps): fixes vitest exports

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ export {
 
 export { remixVitestSetup } from './remix/setup';
 
+export * from 'vitest-axe';
+
+export * as vitestLibrary from 'vitest';
+
 export { renderComponent, auditComponentA11y } from './remix/utils';
 
 export * as browserLibrary from '@testing-library/react';
