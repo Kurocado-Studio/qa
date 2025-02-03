eff04f552125ae9a23cce0ee87a1faa7435058e2
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-02-03 11:15:45 -0600

    fix(config): vitestConfig

--- src/common/vitestConfig.ts
+++ src/common/vitestConfig.ts
@@ -1,12 +1,7 @@
-import path, { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import type { ViteUserConfig } from 'vitest/config';
 
 export interface VitestConfig extends ViteUserConfig {}
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export const vitestConfig: VitestConfig = {
   plugins: [tsconfigPaths()],
@@ -17,12 +12,5 @@ export const vitestConfig: VitestConfig = {
     },
     environment: 'node',
     globals: true,
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src'),
-      '@src': path.resolve(__dirname, './src'),
-      '@app': path.resolve(__dirname, './app'),
-    },
   },
 };
