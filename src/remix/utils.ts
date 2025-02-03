3fb14f5802cc2b7493d21281a5eb68b31aa36883
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-02-03 11:42:40 -0600

    fix(config): auditComponentA11y

--- src/remix/utils.ts
+++ src/remix/utils.ts
@@ -1,5 +1,4 @@
 import { render } from '@testing-library/react';
-import { get } from 'lodash-es';
 import type React from 'react';
 import { axe } from 'vitest-axe';
 
@@ -16,6 +15,6 @@ export const auditComponentA11y = async (
   Component: React.ReactElement,
   wrapper?: ComponentWrapper,
 ): Promise<ReturnType<typeof axe>> => {
-  const container = get(renderComponent(Component, wrapper), ['container'], '');
+  const { container } = renderComponent(Component, wrapper);
   return axe(container);
 };
